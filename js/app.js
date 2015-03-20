104a9ead359da5e6f240c6639f180cb4264c4e26
kristenbasco <kristenbasco@knights.ucf.edu>
2015-03-20 19:28:32 -0400

    front page images are responsive

--- js/app.js
+++ js/app.js
@@ -17,17 +17,17 @@ $(document).ready(function($) {
     new ScrollScene({triggerElement: "#parallax1"})
       .setTween(TweenMax.from("#parallax1 > div", 1, {top: "-80%", ease: Linear.easeNone}))
       .addTo(controller)
-      .addIndicators({zindex: 1, suffix: "1"});
+    
 
     new ScrollScene({triggerElement: "#parallax2"})
       .setTween(TweenMax.from("#parallax2 > div", 1, {top: "-80%", ease: Linear.easeNone}))
       .addTo(controller)
-      .addIndicators({zindex: 1, suffix: "2"});
+   
 
     new ScrollScene({triggerElement: "#parallax3"})
       .setTween(TweenMax.from("#parallax3 > div", 1, {top: "-80%", ease: Linear.easeNone}))
       .addTo(controller)
-      .addIndicators({zindex: 1, suffix: "3"});
+     
 
     $(window).load(function() {
           var options =
